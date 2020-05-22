471fce8cf9c766b14d1dce85eafcf84fd3bebd80
Jean Matheus Vassalo Ferreira <jeanvassaloferreira@gmail.com>
2020-05-21 21:46:33 -0300

    navbar

--- frontend/faketwitter/src/App.js
+++ frontend/faketwitter/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+
+import Home from './pages/Home'
+import Tweets from './pages/Tweets';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/Tweets" component={Tweets}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
